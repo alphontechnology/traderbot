3734a04985ffca23f3aab9c3d2b014886cb455c9
alphon <alphon@MacBook-Pro-de-Gabriel.local>
2021-09-10 20:22:27 -0300

    error middlewares

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,21 +2,27 @@ const express = require('express'); // load express
 const cors = require('cors'); // CORS = cross-origin resources sharing -> middleware security allow front send requests
 const helmet = require('helmet'); // middleware security against 11 attacks
 const app = express(); // create app loading express
-
+require('express-async-errors'); // async errors
 
 // configs middlewares
 app.use(cors());
 app.use(helmet());
 app.use(express.json()); // middleware body parsing
 
-
 // proccess middlewares
 app.use('/middleware', (req, res, next) => {
     res.send('hello middleware process!!');
 })
 
-app.use('/', (req, res, next) => {
+// middleware error test
+app.use('/erro', (req, res, next) => {
+    throw new Error('Error ok...');
+})
+
+app.use('/', (req, res, next) => { // always last root
     res.send('hello world!');
 })
+
+app.use(require('./middlewares/errorMiddleware')); // require error
 
 module.exports = app; // exports app
---
+++ backend/src/middlewares/errorMiddleware.js
@@ -0,0 +1,4 @@
+// middleware error pattern
+module.exports = (error, req, res) => {
+    console.error(error);
+}
