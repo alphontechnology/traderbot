2846ec453d6284e937fbf326f3122c5c628e30e6
alphon <alphon@MacBook-Pro-de-Gabriel.local>
2021-09-10 21:01:30 -0300

    login logout

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,17 +6,24 @@ require('express-async-errors'); // async errors
 
 // configs middlewares
 app.use(cors());
+
 app.use(helmet());
+
 app.use(express.json()); // middleware body parsing
 
-// proccess middlewares
-app.use('/middleware', (req, res, next) => {
-    res.send('hello middleware process!!');
+// login
+app.post('/login', (req, res, next) => { 
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if(email === 'contato@email.com' && password === '111')
+    res.sendStatus(200); // auth ok
+    else res.sendStatus(401); // denied access
 })
 
-// middleware error test
-app.use('/erro', (req, res, next) => {
-    throw new Error('Error ok...');
+// logout
+app.post('/logout', (req, res, next) => {
+    req.sendStatus(200);
 })
 
 app.use('/', (req, res, next) => { // always last root
@@ -25,4 +32,4 @@ app.use('/', (req, res, next) => { // always last root
 
 app.use(require('./middlewares/errorMiddleware')); // require error
 
-module.exports = app; // exports app
+module.exports = app; // exports app 
